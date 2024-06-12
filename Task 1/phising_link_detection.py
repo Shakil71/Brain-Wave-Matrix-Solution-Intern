1fb824d0ad3f03e7430a014355d41a2aeec61061
Shakil71 <mdalaminshakil31@gmail.com>
2024-06-12 10:40:49 +0600

    Phising_Link_Generator

---
+++ Task 1/phising_link_detection.py
@@ -0,0 +1,18 @@
+import re
+
+def is_phishing(url):
+    patterns = [
+        r"https?://(?:www\.)?([^\s]+)\.([^\s/]{2,}|[a-z]{2,})(/[^\s]*)?"
+    ]
+
+    for pattern in patterns:
+        if re.search(pattern, url):
+            return True
+
+    return False
+input_url = input("Please enter the URL to check for phishing: ")
+
+if is_phishing(input_url):
+    print(f"The URL '{input_url}' might be a phishing link.")
+else:
+    print(f"The URL '{input_url}' is not flagged as a phishing link.")
